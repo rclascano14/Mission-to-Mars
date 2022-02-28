4b5184b26e585119f9ee65e9f76666114650572a
rcharleslascano <lascanorc@gmail.com>
2022-02-28 16:24:33 -0600

    10.5.2

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
-
 # Set up Flask
 app = Flask(__name__)
 
@@ -23,7 +22,6 @@ def scrape():
    mars_data = scraping.scrape_all()
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
-
 # Update the Database
 mars.update_one({}, {"$set":mars_data}, upsert=True)
 
